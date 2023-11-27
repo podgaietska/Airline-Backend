3817af561edfd4d0b177f458e2b65a907c4980be
podgaietska <podgaietska.m@gmail.com>
2023-11-26 19:53:11 -0700

    added searching by departure, arrival airports and date

--- src/main/java/edu/ensf480/airline/controller/FlightController.java
+++ src/main/java/edu/ensf480/airline/controller/FlightController.java
@@ -2,9 +2,12 @@ package edu.ensf480.airline.controller;
 
 import edu.ensf480.airline.model.Flight;
 import edu.ensf480.airline.repository.FlightRepository;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -20,6 +23,19 @@ public class FlightController {
     @GetMapping("/all")
     public ResponseEntity<List<Flight>> getAllFlights(){
         List<Flight> flights = flightRepository.findAll();
+        return ResponseEntity.ok(flights);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<?> searchFlights(@RequestParam String departureAirport,
+                                                      @RequestParam String arrivalAirport,
+                                                      @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+        List<Flight> flights = flightRepository.findByDepartureAirportAndArrivalAirportAndFlightDate(departureAirport, arrivalAirport, date);
+
+        if(flights.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Sorry, no flights found for the given parameters.");
+        }
+
         return ResponseEntity.ok(flights);
     }
 
--- src/main/java/edu/ensf480/airline/repository/FlightRepository.java
+++ src/main/java/edu/ensf480/airline/repository/FlightRepository.java
@@ -3,6 +3,10 @@ package edu.ensf480.airline.repository;
 import edu.ensf480.airline.model.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface FlightRepository extends JpaRepository<Flight, Long> {
     //basic CRUD operations
+    List<Flight> findByDepartureAirportAndArrivalAirportAndFlightDate(String departureAirport, String arrivalAirport, LocalDate date);
 }
